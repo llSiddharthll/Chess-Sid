81adf96c69eb09f7401131a0997a7e233c93d85e
llSiddharthll <siddharthgreat443@gmail.com>
2024-02-15 07:21:52 +0530

    update

--- Chess-Sid/settings.py
+++ Chess-Sid/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'sid', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'chessapp', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
