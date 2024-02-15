1da05e5302c12b76df6c4f7f694e21170434ac9c
llSiddharthll <siddharthgreat443@gmail.com>
2024-02-16 03:38:58 +0530

    ok

--- Chess-Sid/asgi.py
+++ Chess-Sid/asgi.py
@@ -9,7 +9,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chess-Sid.settings')
 vercel_env = os.environ.get('VERCEL_ENV', None)
 
 if vercel_env == 'true':
-    application = get_asgi_application()
+    app = ProtocolTypeRouter({
+        "http": get_asgi_application(),  # Make sure this is correctly configured
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        ),
+    })
 else:
     application = ProtocolTypeRouter({
         "http": get_asgi_application(),  # Make sure this is correctly configured
