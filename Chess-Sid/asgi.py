5ec0999af9dd19043cb76b905b4d3866b5a9a910
llSiddharthll <siddharthgreat443@gmail.com>
2024-02-16 03:35:58 +0530

    ok

--- Chess-Sid/asgi.py
+++ Chess-Sid/asgi.py
@@ -2,15 +2,20 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chessapp.routing import websocket_urlpatterns  # Import your routing module
+from chessapp.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chess-Sid.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  # Make sure this is correctly configured
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-})
+vercel_env = os.environ.get('VERCEL_ENV', None)
+
+if vercel_env == 'true':
+    application = get_asgi_application()
+else:
+    application = ProtocolTypeRouter({
+        "http": get_asgi_application(),  # Make sure this is correctly configured
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        ),
+    })
