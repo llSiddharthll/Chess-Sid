8973ea30da60608dab9d54361ada44e23696d539
llSiddharthll <siddharthgreat443@gmail.com>
2024-02-15 07:42:26 +0530

    n

--- Chess-Sid/settings.py
+++ Chess-Sid/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sxqo(b5m!(+rvzu)r-y_!%j#3s0%q3738vcuw#!86^3cjdhgi5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
